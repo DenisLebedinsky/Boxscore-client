0e471ea07af5aceedb70e1f9216a8c5281da0b67
Den Lebedinsky <Den.Lebedinsky@gmail.com>
2019-05-17 17:45:56 +0300

    last change

--- src/socketio/index.ts
+++
@@ -1,8 +0,0 @@
-import io from 'socket.io-client';
-
-const socket = io('http://localhost');
-const subscribeToUpdate = () => {
-    socket.on('timer', console.log(''));
-}
-
-export default subscribeToUpdate;
